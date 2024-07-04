98767e4c3d8666f8d0df60729fc40aa6960c465b
Eden <11130207@dcsh.tp.edu.tw>
2024-07-04 11:30:09 +0800

    3

--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 urlpatterns=[
     path('', MessageList.as_view(), name='msg_list'),#空路徑(/message) 留言列表
-    path('<int:pk>/', MessageView.as_view(), name='msg_view'),#
+    path('<int:pk>/', MessageView.as_view(), name='msg_view'),#留言內容
     path('create/', MessageCreate.as_view(), name='msg_create'),#建立留言
+    path('<int:pk>/delete/', MessageDelete.as_view(), name='msg_delete'),#刪除留言
 ]
--- web/views.py
+++ web/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import *
+from django.contrib.auth.mixins import LoginRequiredMixin #檢測用戶是否登入
 
 # Create your views here.
 
@@ -14,3 +15,7 @@ class MessageCreate(CreateView):
     model = Message
     fields = ['user', 'subject', 'content'] #顯示'user','subject','content'
     success_url = reverse_lazy('msg_list')#新增後導向留言列表(/message)
+# 刪除留言
+class MessageDelete(LoginRequiredMixin,DeleteView):#LoginRequiredMixin需要登入
+    model = Message
+    success_url = reverse_lazy('msg_list')#刪除後導向留言列表(/message)
